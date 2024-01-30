fd85e5a7be1ec238081722025a9321c600705048
Raviranjan Mishra <ravimishra.rm9558@gmail.com>
2024-01-30 13:44:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from"react";
 
 function App() {
+
+  const [username, setUserName] = useState("");
+  const [number, setnumber] = useState();
+  const [store, setStore] = useState([]);
+
+function  handleClick(){
+  setStore([...store, {username, number}]);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Phone Book</h2>
+        <label htmlFor="name">Enter Name </label> 
+         <input type="text" placeholder="Enter your name " value={username} onChange={(e)=>setUserName(e.target.value)} />< br /> <br />
+
+         <label htmlFor = "Number"> Phone Number :</label>
+         <input ype="number" placeholder="Add Phone Number" value={number} onChange={(e) => setnumber(e.target.value)}/>  <br/>< br/>
+
+         <button className="Add-btn" type="submit" onClick={handleClick}>Add contact </button> <br /> <br/>
+
+          <h2>Contacts</h2>
+          <ul>
+            {
+              store.map((item,i) => {
+                return <li key={i}>{item.username}  -  {item.number}</li>
+            })}
+          </ul>
+
     </div>
   );
 }
